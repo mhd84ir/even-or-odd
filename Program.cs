98858a90eef907dcc343d34ad581a6b5adf546fb
unknown <mhd84.ir@gmail.com>
2022-03-23 02:42:14 +0430

    even or odd

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("write your mind");
+int input;
+input = Convert.ToInt32(Console.ReadLine());
+if (input % 2 == 0 && input > 0)
+{
+    System.Console.WriteLine("adad zog ast");
+}
+else if (input % 2 == 0 && input < 0)
+{
+    System.Console.WriteLine("adad zog ast");
+}
+else if (input == 0)
+{
+    System.Console.WriteLine("adad zog ast");
+}
+else
+{
+    System.Console.WriteLine("fard ast");
+}
